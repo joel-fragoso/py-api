b6835903f3a45a40b7da466bde0659eb7e172271
Joel Fragoso <joelfragoso85@icloud.com>
2022-08-28 16:30:24 -0300

    :sparkles: feat: adiciona as configurações, entidades e os repositórios

---
+++ src/infra/repo/faker.py
@@ -0,0 +1,22 @@
+# pylint: disable=E1129
+from src.infra.config import DBConnectionHandler
+from src.infra.entities import Users
+
+
+class FakerRepo:
+    """A simple repository"""
+
+    @classmethod
+    def insert_user(cls):
+        """something"""
+
+        with DBConnectionHandler() as db_connection:
+            try:
+                new_user = Users(name="John Doe", password="secret")
+                db_connection.session.add(new_user)
+                db_connection.session.commit()
+            except:
+                db_connection.session.rollback()
+                raise
+            finally:
+                db_connection.session.close()
